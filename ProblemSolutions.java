aed3cfa2aa1cb101f6353f85e3b6fd271220adca
mqazi5 <141670970+mqazi5@users.noreply.github.com>
2025-04-22 21:23:35 -0500

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   ADD YOUR NAME / SECTION NUMBER HERE
+ *   Mohammad Qazi / SECTION 001
  *
  *   This java file contains the problem solutions of canFinish and
  *   numGroups methods.
